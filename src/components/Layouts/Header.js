70c09e246bc1394887d2089f5a4c7a1ea8512a29
boymtp12 <86153679+boymtp12@users.noreply.github.com>
2023-08-19 01:18:37 +0700

    update header + handle search
    update header + handle search

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../../asset/logo.png";
 import { Link } from "react-router-dom";
-import Search from "../Section/Search";
 import DropdownLoggedOut from "../Elements/DropdownLoggedOut";
 import DropdownLoggedIn from "../Elements/DropdownLoggedIn";
+import { IMG } from "../../asset/img";
+import { Box, IconButton, InputBase } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useDispatch } from "react-redux";
+import { changeDataFeatureProduct } from "../../redux/reducer_action/BaseReducerAction";
+import { toast } from "react-toastify";
 //import axios from "axios";
 const Header = () => {
-  const [show, setshow] = useState(false);
+  const dispatch = useDispatch();
   const [dropdown, setDropdown] = useState(false);
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem("darkMode")) || false
   );
   const token = JSON.parse(sessionStorage.getItem("token"));
 
+  const [searchTerm, setSearchTerm] = useState("");
   //const [cardListLength, setCardListLength] = useState(0);
 
   // const getTokenFromSessionStorage = () => {
@@ -28,7 +34,7 @@ const Header = () => {
   //         Authorization: `Bearer ${token}`,
   //       };
   //       const response = await axios.get("http://localhost:8080/cart-size", { headers });
-        
+
   //       if (response.status === 200) {
   //         const cartSize = response.data; // Giả sử kết quả trả về là số cart size, chẳng hạn 4
   //         setCardListLength(cartSize); // Cập nhật giá trị state cardListLength
@@ -37,9 +43,34 @@ const Header = () => {
   //       console.error("Failed to get cart size:", error);
   //     }
   //   };
-  
+
   //   getCartSize();
   // }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm !== "") {
+      fetch(`http://localhost:8080/product?productName=${searchTerm}`)
+        .then((response) => response.json())
+        .then((rs) => {
+          dispatch(changeDataFeatureProduct([...rs]));
+          toast.success("Search successfull");
+        })
+        .catch((err) => {
+          toast.error("Search failed");
+        });
+    } else {
+      fetch(`http://localhost:8080/product`)
+        .then((response) => response.json())
+        .then((rs) => {
+          dispatch(changeDataFeatureProduct([...rs]));
+          toast.success("Search successfull");
+        })
+        .catch((err) => {
+          toast.error("Search failed");
+        });
+    }
+  };
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -54,30 +85,53 @@ const Header = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <Link to="/" className="flex items-center">
-            <img src={Logo} className="h-8 mr-2" alt="Frontendgyaan Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              E-commerce Website
-            </span>
+            <img
+              src={IMG.logo}
+              style={{ width: "60px", borderRadius: "50%" }}
+              alt="Frontendgyaan Logo"
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                marginLeft: "16px",
+              }}
+              className="bg-white border-gray-200 dark:bg-gray-800"
+            >
+              <span style={{ fontSize: "18px", fontWeight: "bold" }}>
+                HỆ THỐNG SIÊU THỊ HÀNG NHẬT NỘI ĐỊA
+              </span>
+              <span style={{ fontSize: "12px", fontWeight: "bold" }}>
+                SAKUKO JAPANESE STORE
+              </span>
+            </div>
           </Link>
           <div className="flex items-center ">
-            <span
-              onClick={() => setDarkMode(!darkMode)}
-              className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"
-            ></span>
-            <span
-              onClick={() => setshow(!show)}
-              className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"
-            ></span>
+            <form onSubmit={(e) => handleSearch(e)}>
+              <Box
+                style={{ border: "1px solid #ccc", margin: "0 16px" }}
+                display="flex"
+                backgroundColor="#f7f7f7"
+                borderRadius="3px"
+              >
+                <InputBase
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  sx={{ ml: 2, flex: 1 }}
+                  placeholder="Search"
+                />
+                <IconButton type="submit" sx={{ padding: "4px" }}>
+                  <SearchIcon />
+                </IconButton>
+              </Box>
+            </form>
             <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
               <span className="text-2xl bi bi-cart-fill relative">
-                <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full">
-                  
-                </span>
+                <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full"></span>
               </span>
             </Link>
             <span
               onClick={() => setDropdown(!dropdown)}
-              style={{position: 'relative'}}
+              style={{ position: "relative" }}
               className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-person-circle"
             >
               {dropdown &&
@@ -87,11 +141,13 @@ const Header = () => {
                   <DropdownLoggedOut setDropdown={setDropdown} />
                 ))}
             </span>
+            <span
+              onClick={() => setDarkMode(!darkMode)}
+              className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"
+            ></span>
           </div>
         </div>
       </nav>
-
-      {show && <Search setshow={setshow} />}
     </>
   );
 };
